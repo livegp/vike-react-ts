cc5a0f8c068b783b9ba487fa769590bf38580661
Pishta Oleksandr <live.gp@gmail.com>
2024-11-14 22:38:56 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import vike from 'vike/plugin';
 import { defineConfig, loadEnv } from 'vite';
 import { imagetools } from 'vite-imagetools';
 import biomePlugin from 'vite-plugin-biome';
+import hashedFaviconsPlugin from 'vite-plugin-hashed-favicons';
 import { ViteMinifyPlugin } from 'vite-plugin-minify';
+import ogPlugin from 'vite-plugin-open-graph';
 import { reactClickToComponent } from 'vite-plugin-react-click-to-component';
 import Sitemap from 'vite-plugin-sitemap';
 import { svgSpritemap } from 'vite-plugin-svg-spritemap';
@@ -44,8 +46,8 @@ export default defineConfig(({ mode }) => {
         plugins: [['@swc/plugin-emotion', {}]],
       }),
       ViteMinifyPlugin({}),
-      reactClickToComponent(),
       Sitemap(),
+      reactClickToComponent(),
       biomePlugin({
         mode: 'check',
         files: '.',
@@ -65,6 +67,14 @@ export default defineConfig(({ mode }) => {
           proxy: false,
         },
       ),
+      svgSpritemap({
+        pattern: 'assets/icons/*.svg',
+        prefix: '',
+        filename: 'spritemap.svg',
+        currentColor: false,
+        svgo: true,
+        emit: false,
+      }),
       imagetools({
         defaultDirectives: (): URLSearchParams => {
           return new URLSearchParams({
@@ -77,13 +87,97 @@ export default defineConfig(({ mode }) => {
           dir: 'node_modules/.cache/imagetools',
         },
       }),
-      svgSpritemap({
-        pattern: 'assets/icons/*.svg',
-        prefix: '',
-        filename: 'spritemap.svg',
-        currentColor: false,
-        svgo: true,
-        emit: false,
+      hashedFaviconsPlugin('assets/icons/vike.svg', {
+        version: '1.0',
+        lang: 'en-US',
+        appName: 'vite-react-ts',
+        appShortName: 'vite-react-ts',
+        appDescription: 'the starting template of the project',
+        developerName: 'Oleksandr Pishta',
+        // biome-ignore lint/style/useNamingConvention: <explanation>
+        developerURL: 'https://livegp.github.io',
+        scope: '/',
+        // biome-ignore lint/style/useNamingConvention: <explanation>
+        start_url: '/?homescreen=1',
+        loadManifestWithCredentials: false,
+        manifestMaskable: true,
+        manifestRelativePaths: false,
+        background: '#fff',
+        // biome-ignore lint/style/useNamingConvention: <explanation>
+        theme_color: '#fff',
+        appleStatusBarStyle: 'black-translucent',
+        cacheBustingQueryParam: null,
+        dir: 'auto',
+        display: 'standalone',
+        orientation: 'any',
+        preferRelatedApplications: false,
+        relatedApplications: undefined,
+        // biome-ignore lint/style/useNamingConvention: <explanation>
+        pixel_art: false,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          favicons: true,
+          windows: true,
+          yandex: true,
+        },
+      }),
+      ogPlugin({
+        basic: {
+          title: 'vike-react-ts',
+          type: 'image.png',
+          image: {
+            url: 'https://livegp.github.io/vike-react-ts/vike.png',
+            // secureUrl: undefined,
+            type: 'image/png',
+            width: 900,
+            height: 440,
+            alt: 'Vike Image',
+          },
+          url: 'https://livegp.github.io/vike-react-ts/',
+          description: 'the starting template of the project',
+          determiner: 'auto',
+          locale: 'en_US',
+          localeAlternate: ['en_US', 'ru_RU'],
+          siteName: 'vike-react-ts',
+          // video: undefined,
+        },
+        twitter: {
+          card: 'summary_large_image',
+          site: '@live_gp',
+          siteId: '118488504',
+          creator: '@live_gp',
+          creatorId: '118488504',
+          description: 'the starting template of the project',
+          title: 'vike-react-ts',
+          image: 'https://livegp.github.io/vike-react-ts/vike.png',
+          imageAlt: 'Vike Image',
+          // player: undefined,
+          // playerWidth: undefined,
+          // playerHeight: undefined,
+          // playerStream: undefined,
+          // app: {
+          //   name: {
+          //     iphone: undefined,
+          //     ipad: undefined,
+          //     googleplay: undefined,
+          //   },
+          //   id: {
+          //     iphone: undefined,
+          //     ipad: undefined,
+          //     googleplay: undefined,
+          //   },
+          //   url: {
+          //     iphone: undefined,
+          //     ipad: undefined,
+          //     googleplay: undefined,
+          //   },
+          // },
+        },
+        facebook: {
+          appId: 100000506117116,
+        },
       }),
     ],
   };
